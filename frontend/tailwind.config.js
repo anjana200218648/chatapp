7854af5255a495ede134b75c864ebe71bd047ac1
H.H.H.Anjana <146448020+anjana200218648@users.noreply.github.com>
2024-12-25 21:00:20 +0530

    start page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,6 @@
+import daisyui from 'daisyui'
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,5 +10,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 }
